191b4e984ce36d8a1da6f490b5d8d1040aca8cdb
vilay <kandi.vilay09@gmail.com>
2020-08-03 15:16:49 +0530

    Feat : update of wkt Feat : bounding box

--- src/main/java/com/strandls/geoentities/services/impl/GeoentitiesServicesImpl.java
+++ src/main/java/com/strandls/geoentities/services/impl/GeoentitiesServicesImpl.java
@@ -3,6 +3,7 @@
  */
 package com.strandls.geoentities.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -16,6 +17,7 @@ import com.strandls.geoentities.pojo.GeoentitiesCreateData;
 import com.strandls.geoentities.services.GeoentitiesServices;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 import com.vividsolutions.jts.io.WKTWriter;
 
@@ -54,6 +56,23 @@ public class GeoentitiesServicesImpl implements GeoentitiesServices {
 	}
 
 	@Override
+	public Geoentities updateGeoenties(Long geoId, String wkt) {
+
+		try {
+			WKTReader reader = new WKTReader(geoFactory);
+			Geometry topology = reader.read(wkt);
+			Geoentities geoEntities = geoentitiesDao.findById(geoId);
+			if(geoEntities == null) return null;
+			geoEntities.setTopology(topology);
+			geoEntities = geoentitiesDao.update(geoEntities);
+			return geoEntities;
+		} catch (ParseException e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	@Override
 	public List<Geoentities> readPlaceName(String placename) {
 		List<Geoentities> result = geoentitiesDao.findByNameLike(placename);
 		return result;
@@ -67,4 +86,32 @@ public class GeoentitiesServicesImpl implements GeoentitiesServices {
 		return new GeoentitiesCreateData(result.getPlaceName(), wktData);
 	}
 
+	@Override
+	public List<List<Double>> getBoundingBox(Long id) {
+
+		Geoentities geoEntity = geoentitiesDao.findById(id);
+		Geometry topology = geoEntity.getTopology();
+		Geometry envelop = topology.getEnvelope();
+
+		Double  top = envelop.getCoordinates()[0].x, 
+				left = envelop.getCoordinates()[0].y,
+				bottom = envelop.getCoordinates()[3].x, 
+				right = envelop.getCoordinates()[3].y;
+
+		List<List<Double>> boundingBox = new ArrayList<List<Double>>();
+
+		List<Double> topLeft = new ArrayList<Double>();
+		List<Double> bottomRight = new ArrayList<Double>();
+
+		topLeft.add(top);
+		topLeft.add(left);
+
+		bottomRight.add(bottom);
+		bottomRight.add(right);
+
+		boundingBox.add(topLeft);
+		boundingBox.add(bottomRight);
+
+		return boundingBox;
+	}
 }
